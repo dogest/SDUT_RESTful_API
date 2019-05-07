59f4fa6736db1490366bf2ffbd00277e0537f506
MeiK <meik2333@gmail.com>
2019-05-07 15:42:23 +0800

    添加上课时间的解析

--- spider/schedule/schedule.py
+++ spider/schedule/schedule.py
@@ -33,6 +33,51 @@ def sort_schedule(raw_data: list) -> list:
     )
 
 
+def list_duration_range(duration_str: str) -> list:
+    """
+    将字符串的上课时间信息解析为列表
+    >>> list_duration_range('1-2')
+    [1, 2]
+    >>> list_duration_range('10-17')
+    [10, 11, 12, 13, 14, 15, 16, 17]
+    >>> list_duration_range('9')
+    [9]
+    >>> list_duration_range('10-12(双)')
+    [10, 12]
+    >>> list_duration_range('1-3,7-9')
+    [1, 2, 3, 7, 8, 9]
+    >>> list_duration_range('10-12(双),13-17')
+    [10, 12, 13, 14, 15, 16, 17]
+    """
+    rlist = []
+    # 首先按照逗号分割多个情况
+    if ',' in duration_str:
+        durations = duration_str.split(',')
+    else:
+        durations = [duration_str]
+    # 判断是否为单双周的课程
+    for item in durations:
+        if '-' not in item:
+            rlist.append(int(item))
+            continue
+        if '(单)' in item:
+            item = item[:-3]
+            l, r = [int(x) for x in item.split('-')]
+            if l % 2 == 0:
+                l += 1
+            rlist += list(range(int(l), int(r) + 1, 2))
+        elif '(双)' in item:
+            item = item[:-3]
+            l, r = [int(x) for x in item.split('-')]
+            if l % 2 != 0:
+                l += 1
+            rlist += list(range(int(l), int(r) + 1, 2))
+        else:
+            l, r = [int(x) for x in item.split('-')]
+            rlist += list(range(int(l), int(r) + 1))
+    return rlist
+
+
 def fields_map(raw_data: dict) -> dict:
     """ 将教务管理系统的拼音字段转换为英文字段 """
     data = {}
@@ -41,7 +86,10 @@ def fields_map(raw_data: dict) -> dict:
     for item in raw_data['kbList']:
         t = {}
         t['duration_of_class'] = item['jcor']
+        t['duration_of_class_list'] = list_duration_range(
+            t['duration_of_class'])
         t['duration_of_week'] = item['zcd'].replace('周', '')
+        t['duration_of_week_list'] = list_duration_range(t['duration_of_week'])
         t['classroom'] = item['cdmc']
         t['class_name'] = item['kcmc']
         t['day_of_week'] = item['xqj']
@@ -55,6 +103,7 @@ def fields_map(raw_data: dict) -> dict:
         t['class_name'] = item['kcmc']
         t['teacher_name'] = item['xm']
         t['duration_of_week'] = item['qsjsz'].replace('周', '')
+        t['duration_of_week_list'] = list_duration_range(t['duration_of_week'])
         data['extra'].append(t)
 
     data['note'] = []
