b8b7782cbb808e9e6a62bd4388e6a0b92681f5af
MeiK <meik2333@gmail.com>
2019-06-03 13:49:44 +0800

    修复无法获得校园卡余额的问题

--- spider/card/balance.py
+++ spider/card/balance.py
@@ -1,3 +1,6 @@
+import time
+import json
+
 import aiohttp
 from bs4 import BeautifulSoup
 
@@ -10,15 +13,25 @@ async def balance(cookies: dict):
     # 校园卡平台只能通过 IP 访问，因此这里要使用 Unsafe CookieJar
     async with aiohttp.ClientSession(cookie_jar=aiohttp.CookieJar(unsafe=True)) as session:
         await auth_ehall(session, cookies)
-        await auth_card(session)
+        # 校园卡平台无法访问，换用其他接口
+        # await auth_card(session)
 
-        async with session.get('http://211.64.27.136/SelfSearch/EcardInfo/UserBaseInfo_Seach.aspx') as resp:
+        url = f'http://ehall.sdut.edu.cn/jsonp/personalRemind/view.do?amp_jsonp_callback=callback&_={int(time.time() * 1000)}'
+
+        async with session.get(url) as resp:
             text = await resp.text()
-        soup = BeautifulSoup(text, 'html.parser')
-        ipts = soup.find_all('input')
+        json_data = json.loads(text[9:-1])
+
+        bal = '获取失败'
+        userid = '获取失败'
+        for item in json_data['infoList']:
+            if item['title'] == '一卡通信息':
+                bal = item['imptInfo'][5:-1]
+                userid = item['subTitle'][3:]
+                break
+
         data = {
-            'userid': ipts[3].get('value'),
-            'name': ipts[4].get('value'),
-            'balance': ipts[9].get('value')[:-2].strip()
+            'userid': userid,
+            'balance': bal
         }
     return data
